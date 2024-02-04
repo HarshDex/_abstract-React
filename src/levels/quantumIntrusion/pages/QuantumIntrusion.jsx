71284792fffa58075711579ff87da8b8e6983ce5
HarshDex <nature4917@gmail.com>
2024-02-04 12:09:43 +0530

    complete : level 9 quantum intrusion is completed

--- src/levels/quantumIntrusion/pages/QuantumIntrusion.jsx
+++ src/levels/quantumIntrusion/pages/QuantumIntrusion.jsx
@@ -3,13 +3,16 @@ import LoadingScreen from '../../../Loading/LoadingScreen';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RESET_LEVEL, SET_IS_ATTEMPTED, SET_iS_CORRECT } from '../../../redux/slice/QuantumIntrusionSlice';
+
+import './QuantumIntrusion.css'
 const QuantumIntrusion = () => {
-    const [loading,setLoading] = useState(true);
+    const [loading,setLoading] = useState(false);
     const {para1,para2,para3} = useSelector((state)=>state.Quantum.quantumIntrusionData)
     const quantumIDCredentials = useSelector((state)=>state.Quantum.quantumIDCredentials);
     const isAttempted = useSelector((state)=>state.Quantum.
     isAttempted);
     const isCorrect = useSelector((state)=>state.Quantum.isCorrect);
+    const question = useSelector((state)=>state.Quantum.quantumIntrusionData.question)
     const [ans,setAns] = useState('');
     const dispatch = useDispatch();
     useEffect(() => {
@@ -41,7 +44,7 @@ const QuantumIntrusion = () => {
                 </div>
                 <div className="quantum-intrusion-container-data">
                     <div className="quantum-intrusion-container-image">
-                        {/* <img src={quantum-intrusionAsset} alt="" /> */}
+                        <img src='https://img.freepik.com/premium-vector/blue-eye-cyber-circuit-future-technology-concept-background_42077-3054.jpg' alt="" />
                     </div>
                     <div className="quantum-intrusion-container-para">
                         <div className="quantum-intursion-para1">
@@ -50,13 +53,14 @@ const QuantumIntrusion = () => {
                         <div className="quantum-intursion-para2">
                             {para2}
                         </div>
-                        <div className="quantum-intursion-para2">
+                        <div className="quantum-intursion-para3">
                             {para3}
                         </div>
                     </div>
                     <div className="quantum-intrusion-code">
                         <pre>{quantumIDCredentials}</pre>
                     </div>
+                    <div className="quantum-intrusion-question">{question}</div>
                     <div className="quantum-intrusion-container-help">
                         <p>Feeling a little bit stuck? Check out our tips and tricks {' '}
                         <Link to='/quantum-intrusion/quantum-intrusion-tips-and-tricks'>
@@ -88,8 +92,8 @@ const QuantumIntrusion = () => {
                             isAttempted 
                             ? (
                                 isCorrect
-                                ? <p>you are correct</p>
-                                : <p>you are wrong</p>
+                                ? <p>Bravo! You've successfully uncovered the specific user information Ninja gained access to. Your cybernetic sanctuary is becoming more secure, but there's always more to explore and fortify.</p>
+                                : <p>Not quite there yet! Keep exploring the code and think about the information that's commonly associated with users. What personal data might Ninja have targeted?.</p>
                             )
                             :(<></>)
                         }
